23d1968f7c3f4e74b58f2ea718ca746e2cf668d5
Elizabeth <elizabeth.martsyn@outlook.com>
2020-03-17 00:31:05 +0200

    first commit

--- myshop/urls.py
+++ myshop/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+
 from shop import views as core_views
 
 urlpatterns = [
-    url('signup/', core_views.signup, name='signup'),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': 'login'}, name='logout'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,16 @@
-from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from django.views.generic.base import View
+
+from shop.forms import LoginForm
+
+
+@login_required
+def home(request):
+    return render(request, 'registration/home.html')
 
 
 def signup(request):
@@ -15,4 +25,45 @@ def signup(request):
             return redirect('home')
     else:
         form = UserCreationForm()
-    return render(request, 'signup.html', {'form': form})
+    return render(request, 'registration/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            pass
+    return render(request, 'registration/login.html', {'form': form})
+
+
+class LoginView(FormView):
+    form_class = LoginForm
+    success_url = '/'
+    template_name = 'registration/login.html'
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return super(LoginView, self).get(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(self.request, username=username, password=password)
+
+        if user is not None:
+            login(self.request, user)
+        return redirect('/')
+
+
+class LogoutView(View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        logout(self.request)
+        return redirect('/')
