9370f10e75331bc631ddc5e1485995e6d6ae3c97
Elizabeth <elizabeth.martsyn@outlook.com>
2020-03-16 23:19:33 +0200

    first commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'myshop/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
