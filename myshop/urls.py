fb8db98142d357849a7d931c17dbf3ea4c1d2c42
Elizabeth <elizabeth.martsyn@outlook.com>
2020-03-23 19:45:26 +0200

    second commit

--- myshop/urls.py
+++ myshop/urls.py
@@ -13,14 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.urls import path
 
 from shop import views as core_views
 
+
 urlpatterns = [
-    url(r'^$', core_views.home, name='home'),
-    url(r'^login/$', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.LogoutView.as_view(), {'next_page': 'login'}, name='logout'),
-    url(r'^signup/$', core_views.signup, name='signup'),
+    path('admin/', admin.site.urls),
+    path('', core_views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(), {'template_name': 'login.html'}, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': 'login'}, name='logout'),
+    path('signup/', core_views.signup, name='signup'),
 ]
--- shop/models.py
+++ shop/models.py
@@ -1,1 +1,62 @@
+import uuid
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class ShopUser(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+
+    date_of_birth = models.DateField(blank=True, null=True)
+
+    is_moderator = models.BooleanField(default=False)
+
+    email_confirmed = models.BooleanField(default=False)
+    token = models.CharField(max_length=32, unique=True)
+
+    def get_full_name(self):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    @staticmethod
+    def generate_token():
+        return str(uuid.uuid4()).replace('-', '')
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = self.generate_token()
+        return super().save(*args, **kwargs)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
